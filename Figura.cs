15e136c2e7ab6fd924553e0d5aec0035ad730530
varka <macma17@hotmail.com>
2019-11-01 22:21:16 -0500

    Se definio correctamente la clase figura

--- Figura.cs
+++ Figura.cs
@@ -2,10 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class iniciarDatos : MonoBehaviour {
+public class Figura {
 
-	
-	
+
+    protected float alturaTr;
+    protected float baseTr;
+    protected float varCirCuad;
+
+    public Figura(float al, float bs)
+    {
+        alturaTr = al;
+        baseTr = bs;
+    }
+
+    public Figura(float vl)
+    {
+        varCirCuad = vl;
+    }
+
+    public virtual void CalcularArea()
+    {
+        //calcula area de la figura (se sobreescribe en el hijo) 
+    }
+
+    public virtual void RenderizarObjeto()
+    {
+        //renderiza la figura (se sobreescribe en el hijo)   
+    }
 	
 	
 }
