1762092f8df575617e6ab818d43c77ea19b47753
varka <macma17@hotmail.com>
2019-11-01 22:21:43 -0500

    definida la clase triangulo

--- Triangulo.cs
+++ Triangulo.cs
@@ -2,9 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class iniciarDatos : MonoBehaviour {
+public class Triangulo : Figura {
+
+    private float altura;
+    private float baseT;
+    private float area;
+
+    public Triangulo(float al, float bs) : base(al, bs)
+    {
+        altura = al;
+        baseT = bs;
+    }
+
+    public override void CalcularArea()
+    {
+        area = ((altura * baseT) / 2);
+        Debug.Log("el área del triangulo corresponde a: " + area);
+    }
+
+    public override void RenderizarObjeto(){
+        //aquí va el método de renderizar objeto
+    }
 
 
-	
-	
 }
